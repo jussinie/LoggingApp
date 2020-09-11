40deae02e3dd08cb54ae205c66aee3249efdd141
jussinie <juissinie@gmail.com>
2020-09-11 18:41:58 +0300

    Transactional added to Post-method

--- src/main/java/com/example/LoggingApplication/LearningLogController.java
+++ src/main/java/com/example/LoggingApplication/LearningLogController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,7 @@ public class LearningLogController {
         return "redirect:/";
     }
 
+    @Transactional
     @GetMapping("/journal")
     public String showJournalEntries(Model model) {
         List<JournalEntry> entries = journalEntryRepository.findAll();
