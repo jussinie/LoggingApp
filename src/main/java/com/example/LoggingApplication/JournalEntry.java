ba5f97e58b0ddcf919154033d8ca708afde21f71
jussinie <juissinie@gmail.com>
2020-09-11 18:26:38 +0300

    Testing if Postgres would work, H2 won't

--- src/main/java/com/example/LoggingApplication/JournalEntry.java
+++ src/main/java/com/example/LoggingApplication/JournalEntry.java
@@ -3,12 +3,14 @@ package com.example.LoggingApplication;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import java.sql.Clob;
 import java.time.LocalDate;
+
 
 @Entity
 @Data
@@ -20,6 +22,7 @@ public class JournalEntry extends AbstractPersistable<Long> {
     private LocalDate date;
     private String author;
     private String title;
+    @Type(type = "org.hibernate.type.TextType")
     @Lob
     private Clob content;
 
